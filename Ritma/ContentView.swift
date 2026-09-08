560cbe81a5719f19e6370a16a81785c4acdf28b4
Rodrigue <jaunetrodrigue@gmail.com>
2026-09-08 12:45:27 +0200

    Initial Commit

---
+++ Ritma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ritma
+//
+//  Created by Rodrigue Jaunet on 08/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ritma/RitmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitmaApp.swift
+//  Ritma
+//
+//  Created by Rodrigue Jaunet on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RitmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
